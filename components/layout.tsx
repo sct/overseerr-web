e6b60ee71b9cbc81bcf9a24c40d3b26f6d503aac
sct <sctsnipe@gmail.com>
2020-12-28 12:01:54 +0000

    Update image fader size and resize images

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,17 +6,19 @@ import ImageFader from './ImageFader';
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen mx-auto max-w-7xl">
-      <ImageFader
-        isDarker
-        backgroundImages={[
-          '/images/mando.jpg',
-          '/images/ww_bg.jpg',
-          '/images/tenet.jpg',
-          '/images/coco.jpg',
-          '/images/avengers.jpg',
-          '/images/moana.jpg',
-        ]}
-      />
+      <div className="absolute inset-x-0 top-0 image-fader">
+        <ImageFader
+          isDarker
+          backgroundImages={[
+            '/images/mando.jpg',
+            '/images/ww_bg.jpg',
+            '/images/tenet.jpg',
+            '/images/coco.jpg',
+            '/images/avengers.jpg',
+            '/images/moana.jpg',
+          ]}
+        />
+      </div>
       <Header />
 
       <main className="z-40 flex-1 px-4 py-6 md:px-6 md:py-12">{children}</main>
